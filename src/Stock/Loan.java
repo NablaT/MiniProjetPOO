858ed78caba7b92dcee82ec10bd45e88e82dfeb6
Rémi Pourtier <pourtier@polytech.unice.fr>
2013-11-24 22:52:24 +0100

    Rajout d'une instance dans la classe Loan (string: description. Pour connaître les raisons pour lesquelles l'emprunteur veut ce matériel). + création de la TODO list

--- src/Stock/Loan.java
+++ src/Stock/Loan.java
@@ -6,7 +6,7 @@ import Living.Borrower;
 import Material.Material;
 
 /**
- * Classe Loan. Un loan se caracterise par une date d'emprunt, une date de retour, l'emprunteur et le materiel emprunté. 
+ * Classe Loan. Un loan se caracterise par une date d'emprunt, une date de retour, l'emprunteur, le materiel emprunté et la description de l'emprunt. 
  * Cette classe permet de garder une trace des emprunts et de pouvoir les controler. En effet grâce à la date de
  * retour, on pourra savoir si un emprunteur est en retard ou non.
  */
@@ -17,6 +17,7 @@ public class Loan {
 	private Date dateBack;
 	private Borrower borrower; 
 	private Material material;
+	private String description;
 	
 	/**
 	 * Constructeur classe Loan. Ce constructeur permet de créer un Loan à partir de deux dates (date d'emprunt et date 
@@ -24,11 +25,13 @@ public class Loan {
 	 * @param dateLoan
 	 * @param dateBack
 	 * @param borrower
+	 * @param description
 	 */
-	public Loan(Date dateLoan, Date dateBack, Borrower borrower){
+	public Loan(Date dateLoan, Date dateBack, Borrower borrower, String description){
 		this.dateLoan=dateLoan;
 		this.dateBack=dateBack;
 		this.borrower=borrower;
+		this.description=description;
 	}
 	
 	/**
@@ -67,6 +70,15 @@ public class Loan {
 	}
 	
 	/**
+	 * Methode getDescription. Cette methode retourne la description de l'emprunt. 
+	 * @return
+	 */
+	
+	public String getDescription(){
+		return this.description;
+	}
+	
+	/**
 	 * Methode setDateLoan. Cette methode remplace la date à laquelle l'emprunt a été fait. 
 	 * @param dateLoan
 	 */
@@ -102,4 +114,12 @@ public class Loan {
 		this.material=material;
 	}
 	
+	/**
+	 * Methode setDescription. Cette methode remplace la description de l'emprunt. 
+	 * @param desc
+	 */
+	
+	public void setDescription(String desc){
+		this.description=desc;
+	}
 }
