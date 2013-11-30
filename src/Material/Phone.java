ca80286c3ced8bab3f6c0b22c9fb30a8f3d8389d
TomVeniat <veniat.tom@etu.unice.fr>
2013-11-30 20:41:47 +0100

    Classe Phone

--- src/Material/Phone.java
+++ src/Material/Phone.java
@@ -1,5 +1,46 @@
 package Material;
 
+/**
+ * Classe de définition de Phone. Phone hérite de Material.
+ * 
+ * @author Tom Veniat
+ *
+ */
 public class Phone extends ComputerDevice {
-
+	
+	/**
+	 * Variable de classe, correspondant au nombre de téléphones existants.
+	 * 
+	 */
+	private static int numberOfPhones;
+	
+	/**
+	 * Constructeur de Phone.
+	 * 
+	 */
+	public Phone(){
+		super.type=Type.PHONE;
+		//id="PHO"+String.valueOf(numberOfPhones);
+		numberOfPhones++;
+	}
+	
+	/**
+	 * Getteur de numberOfPhones.
+	 * 
+	 * @return le nombre de Phone créés.
+	 */
+	public static int getNumberOfPhones(){
+		return numberOfPhones;
+	}
+	
+	/**
+	 * Setteur de numberOfPhones.
+	 * 
+	 * @param number le nombre que l'on veut assigner à numberOfPhone.
+	 */
+	public static void setNumberOfPhone(int number){
+		numberOfPhones=number;
+	}
+	
+	
 }
