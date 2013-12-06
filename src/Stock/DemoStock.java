804a6007c0aac71f1809784e0817dee861169759
Rémi Pourtier <pourtier@polytech.unice.fr>
2013-12-06 02:58:43 +0100

    creation de la classe DemoStock

---
+++ src/Stock/DemoStock.java
@@ -0,0 +1,15 @@
+package Stock;
+
+import java.util.ArrayList;
+
+import Living.Borrower;
+
+public class DemoStock {
+
+	private StockManagement mstock; 
+	private ArrayList<Borrower> borrower; 
+	
+	public DemoStock(){
+		
+	}
+}
